2b2d0c8b1c4f4029c620bc41409963ba4dd59f56
Robert Stanfield <robert@stanfield.it>
2019-07-23 04:29:58 -0400

    store log in ram for speed improvement

--- successrate.sh
+++ successrate.sh
@@ -4,21 +4,26 @@
 # assumes your docker container is named 'storagenode'. If not, pass it as the first argument, e.g.:
 # bash successrate.sh mynodename
 DOCKER_NODE_NAME="${1:-storagenode}"
-LOG="docker logs $DOCKER_NODE_NAME"
+
+LOG_CACHE=$(mktemp -p /dev/shm/)
+LOG_SOURCE="docker logs $DOCKER_NODE_NAME"
 #Log line can be edited using cat for SNO's who wrote their log to a file.
-#LOG="cat /volume1/storj/v3/data/node.log"
+#LOG_SOURCE="cat /volume1/storj/v3/data/node.log"
+
+# Cache log
+$LOG_SOURCE > $LOG_CACHE 2>&1
 
 #Node Success Rates
 
 echo -e "\e[96m========== AUDIT ============= \e[0m"
 #count of successful audits
-audit_success=$($LOG 2>&1 | grep GET_AUDIT | grep downloaded -c)
+audit_success=$(cat $LOG_CACHE | grep GET_AUDIT | grep downloaded -c)
 echo -e "\e[92mSuccessful:           $audit_success \e[0m"
 #count of recoverable failed audits
-audit_failed_warn=$($LOG 2>&1 | grep GET_AUDIT | grep failed | grep -v open -c)
+audit_failed_warn=$(cat $LOG_CACHE | grep GET_AUDIT | grep failed | grep -v open -c)
 echo -e "\e[33mRecoverable failed:   $audit_failed_warn \e[0m"
 #count of unrecoverable failed audits
-audit_failed_crit=$($LOG 2>&1 | grep GET_AUDIT | grep failed | grep open -c)
+audit_failed_crit=$(cat $LOG_CACHE | grep GET_AUDIT | grep failed | grep open -c)
 echo -e "\e[91mUnrecoverable failed: $audit_failed_crit \e[0m"
 #Ratio of Successful to Failed Audits
 if [ $(($audit_success+$audit_failed_crit+$audit_failed_warn)) -ge 1 ]
@@ -38,10 +43,10 @@ echo -e "Success Rate Max:     $audit_success_max"
 
 echo -e "\e[96m========== DOWNLOAD ========== \e[0m"
 #count of successful downloads
-dl_success=$($LOG 2>&1 | grep '"GET"' | grep downloaded -c)
+dl_success=$(cat $LOG_CACHE | grep '"GET"' | grep downloaded -c)
 echo -e "\e[92mSuccessful:           $dl_success \e[0m"
 #Failed Downloads from your node
-dl_failed=$($LOG 2>&1 | grep '"GET"' | grep failed -c)
+dl_failed=$(cat $LOG_CACHE | grep '"GET"' | grep failed -c)
 echo -e "\e[33mFailed:               $dl_failed \e[0m"
 #Ratio of Failed Downloads
 if [ $(($dl_success+$dl_failed)) -ge 1 ]
@@ -54,13 +59,13 @@ echo -e "Success Rate:         $dl_ratio"
 
 echo -e "\e[96m========== UPLOAD ============ \e[0m"
 #count of successful uploads to your node
-put_success=$($LOG 2>&1 | grep '"PUT"' | grep uploaded -c)
+put_success=$(cat $LOG_CACHE | grep '"PUT"' | grep uploaded -c)
 echo -e "\e[92mSuccessful:           $put_success \e[0m"
 #count of rejected uploads to your node
-put_rejected=$($LOG 2>&1 | grep rejected | grep upload -c)
+put_rejected=$(cat $LOG_CACHE | grep rejected | grep upload -c)
 echo -e "\e[33mRejected:             $put_rejected \e[0m"
 #count of failed uploads to your node
-put_failed=$($LOG 2>&1 | grep '"PUT"' | grep failed -c)
+put_failed=$(cat $LOG_CACHE | grep '"PUT"' | grep failed -c)
 echo -e "\e[33mFailed:               $put_failed \e[0m"
 #Ratio of Uploads
 if [ $(($put_success+$put_rejected)) -ge 1 ]
@@ -80,10 +85,10 @@ echo -e "Success Rate:         $put_ratio"
 
 echo -e "\e[96m========== REPAIR DOWNLOAD === \e[0m"
 #count of successful downloads of pieces for repair process
-get_repair_success=$($LOG 2>&1 | grep GET_REPAIR | grep downloaded -c)
+get_repair_success=$(cat $LOG_CACHE | grep GET_REPAIR | grep downloaded -c)
 echo -e "\e[92mSuccessful:           $get_repair_success \e[0m"
 #count of failed downloads of pieces for repair process
-get_repair_failed=$($LOG 2>&1 | grep GET_REPAIR | grep failed -c)
+get_repair_failed=$(cat $LOG_CACHE | grep GET_REPAIR | grep failed -c)
 echo -e "\e[33mFailed:               $get_repair_failed \e[0m"
 #Ratio of GET_REPAIR
 if [ $(($get_repair_success+$get_repair_failed)) -ge 1 ]
@@ -96,10 +101,10 @@ echo -e "Success Rate:         $get_repair_ratio"
 
 echo -e "\e[96m========== REPAIR UPLOAD ===== \e[0m"
 #count of successful uploads of repaired pieces
-put_repair_success=$($LOG 2>&1 | grep PUT_REPAIR | grep uploaded -c)
+put_repair_success=$(cat $LOG_CACHE | grep PUT_REPAIR | grep uploaded -c)
 echo -e "\e[92mSuccessful:           $put_repair_success \e[0m"
 #count of failed uploads repaired pieces
-put_repair_failed=$($LOG 2>&1 | grep PUT_REPAIR | grep failed -c)
+put_repair_failed=$(cat $LOG_CACHE | grep PUT_REPAIR | grep failed -c)
 echo -e "\e[33mFailed:               $put_repair_failed \e[0m"
 #Ratio of PUT_REPAIR
 if [ $(($put_repair_success+$put_repair_failed)) -ge 1 ]
@@ -109,3 +114,5 @@ else
 	put_repair_ratio=0.000%
 fi
 echo -e "Success Rate:         $put_repair_ratio"
+
+rm $LOG_CACHE
